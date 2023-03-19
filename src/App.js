160b201df254af8ea6257a12e2ea047d9d99ed81
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-03-19 17:42:14 +0530

    updated with switches and routers

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Navbar from './components/Navbar'
 import Signin from './components/Signin';
 import ProductList from './components/ProductList'
+import Projects from './components/Projects';
 
 function App() {
   return (
     <>
-    <Navbar></Navbar>
-    <Signin></Signin>
-    <ProductList></ProductList>
+    <Router>
+      <Navbar></Navbar>
+      <Routes>
+    <Route path="/projects" element= {<Projects/>} />
+    <Route path="/signin" element= {<Signin/>} />
+    <Route path="/shop" element= {<ProductList/>} />
+
+      </Routes>
+    </Router>
     </>
   );
 }
