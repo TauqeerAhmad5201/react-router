1420030e8ab015de6b53a2a4366b69b92e51feef
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-03-19 11:03:58 +0530

    added ProductList Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar'
 import Signin from './components/Signin';
+import ProductList from './components/ProductList'
 
 function App() {
   return (
     <>
     <Navbar></Navbar>
     <Signin></Signin>
+    <ProductList></ProductList>
     </>
   );
 }
