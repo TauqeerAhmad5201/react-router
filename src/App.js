098a3108b884aede82a9865c23998355974fe11d
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-03-19 13:45:21 +0530

    added reacrt-rounter-dom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import Navbar from './components/Navbar'
 import Signin from './components/Signin';
 import ProductList from './components/ProductList'
